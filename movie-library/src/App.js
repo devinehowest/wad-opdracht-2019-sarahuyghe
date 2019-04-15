1e325351b2b23ea6803fabd2684a27058ea5f3cb
sarahuyghe <sara.huyghe@student.howest.be>
2019-04-15 10:36:27 +0200

    cleaned up code

--- movie-library/src/App.js
+++ movie-library/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 
 import Movies from "./components/Movies";
@@ -12,61 +11,27 @@ class App extends Component {
 			movies: []
 		};
 		this.apiKey = process.env.REACT_APP_MovieDB_API;
-		// console.log(this.apiKey);
-
-		// fetch(
-		// 	`https://api.themoviedb.org/3/movie/top_rated?api_key=62b6ebfe3d6498b7c85dfc5764245b5d`
-		// )
-		// 	.then(r => r.json())
-		// 	// .then(moviesdb => Object.keys(moviesdb).forEach(this.addDB));
-		// 	// .then(db => )
-		// 	// .then(movies => console.log(movies.results));
-		// 	.then(movies => this.setState({ movies: [...movies.results] }));
-		// Object.keys(movies).forEach(this.addMovie));
 	}
-
-	// addDB = db => {
-	// 	console.log(db);
-	// 	Object.keys(db).forEach(results => console.log(results));
-	// };
-
-	// addMovie = movies => {
-	// 	// console.log(movie.title);
-	// 	// movies.map((movie, i) => console.log(movie.title));
-	// 	movies.map((movie, i) => <Movie movie={movie} key={i} />);
-	// 	// Object.keys(movie).forEach(this.addNewMovie);
-	// };
 
 	addNewMovie = movie => {
 		console.log(movie);
 	};
 
-	componentWillMount;
-
 	handleClick = () => {
-		// const { apiKey } = this.string;
 		fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${this.apiKey}`)
 			.then(r => r.json())
-			// .then(moviesdb => Object.keys(moviesdb).forEach(this.addDB));
-			// .then(db => )
-			// .then(movies => console.log(movies.results));
 			.then(movies => this.setState({ movies: [...movies.results] }));
 	};
 
 	render() {
 		const { movies } = this.state;
 		console.log(movies);
-		// console.log(this.apiKey);
-		// const { title } = this.state;
-		// const { overview } = this.state;
 		return (
 			<>
 				<button onClick={this.handleClick} />
 				{/* <Movies movies={this.state.movies} />; */}
 			</>
 		);
-		// <Movies movies={this.state.movies} />;
-		// <></>;
 	}
 }
 
