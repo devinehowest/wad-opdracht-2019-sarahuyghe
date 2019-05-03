b722fe002abea07e328c67782994ae253965ccba
sarahuyghe <sara.huyghe@student.howest.be>
2019-05-03 10:13:15 +0200

    updated storybook

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -3,7 +3,6 @@ export const ROUTES = {
 	upcoming: "/upcoming",
 	toprated: "/toprated",
 	watchlist: "/watchlist",
-	detail: "",
 	login: "/signin",
 	register: "/signup"
 };
--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -3,19 +3,32 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-import Watchlist from "../components/Watchlist.jsx";
+import MovieWatchlist from "./../components/watchlist/MovieWatchlist";
 
-import MovieWatchlist from "./../components/MovieWatchlist.jsx";
 import MovieWatchList from "./../models/MovieWatchList";
 
-import watchlistItem from "./../components/MovieWatchList.module.css";
+import Movies from "./../components/home/Movies";
+import LoginForm from "./../components/auth/LoginForm";
+import RegisterForm from "./../components/auth/RegisterForm";
 
 const movie = new MovieWatchList(
 	`Hellboy`,
 	`537915`,
-	`https://image.tmdb.org/t/p/w500//bk8LyaMqUtaQ9hUShuvFznQYQKR.jpg`
+	`https://image.tmdb.org/t/p/w500//bk8LyaMqUtaQ9hUShuvFznQYQKR.jpg`,
+	false
+);
+const movieWatched = new MovieWatchList(
+	`Hellboy`,
+	`537915`,
+	`https://image.tmdb.org/t/p/w500//bk8LyaMqUtaQ9hUShuvFznQYQKR.jpg`,
+	true
 );
 
-storiesOf("watchlist", module).add("list", () => (
-	<MovieWatchlist movie={movie} />
-));
+storiesOf(`watchlist`, module)
+	.add(`list`, () => <MovieWatchlist movie={movie} />)
+	.add(`watched movie`, () => <MovieWatchlist movie={movieWatched} />);
+
+storiesOf(`Navigation`, module)
+	.add(`Home`, () => <Movies />)
+	.add(`Login`, () => <LoginForm />)
+	.add(`Register`, () => <RegisterForm />);
